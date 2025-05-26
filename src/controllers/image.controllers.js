d672847d5eb3f7ba40a1a7b458b0164781006c1b
Mohsanul786 <mdmohsan2407@gmail.com>
2025-05-26 08:24:43 +0530

    image controller added

--- src/controllers/image.controllers.js
+++ src/controllers/image.controllers.js
@@ -1,14 +1,17 @@
 import { Image } from "../models/image.model.js";
 import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import fs from "fs";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { Album } from "../models/album.model.js";
 
 const uploadImage = asyncHandler(async (req, res) => {
   // extract file details
   // validations
   // cloudinary save
   const { albumId } = req.params;
-  const { tags, isFavorite, comments } = req.body;
+  const { tags, isFavorite, commentText } = req.body;
   const file = req.file;
   if (!file) {
     throw new ApiError(401, "No file found");
@@ -19,7 +22,7 @@ const uploadImage = asyncHandler(async (req, res) => {
     fs.unlinkSync(path); // Delete the temp file
     throw new ApiError(401, "Image size should not exceed 5MB");
   }
-  const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
+  const allowedTypes = ["image/jpeg", "image/png", "image/webp", "image/gif"];
 
   if (!allowedTypes.includes(mimetype)) {
     //  Delete the file if invalid
@@ -41,11 +44,100 @@ const uploadImage = asyncHandler(async (req, res) => {
   const user = await Image.create({
     albumId,
     name: originalname.split(".")[0],
-    coverImage: coverImage?.url || "",
-    email,
-    password,
-    username: username.toLowerCase(),
+    imageUrl: uploadedImage?.url,
+    tags,
+    isFavorite,
+    size: size / (1024 * 1024),
+    comments: { user: req.user._id, text: commentText },
   });
+  return res
+    .status(201)
+    .json(new ApiResponse(200, user, "Image uploaded and saved successfully"));
 });
 
-export { uploadImage };
+const favoriteImage = asyncHandler(async (req, res) => {
+  const { isFavorite } = req.body;
+  const { imageId } = req.params;
+
+  const image = await Image.findOne({ imageId });
+
+  if (!image) {
+    throw new ApiError(404, "Image not found");
+  }
+
+  image.isFavorite = isFavorite;
+  await image.save(); // Save the updated document
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, image, "Image favorite status updated"));
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+  const { comment } = req.body;
+  const { imageId } = req.params;
+
+  const image = await Image.findOne({ imageId });
+
+  if (!image) {
+    throw new ApiError(404, "Image not found");
+  }
+  image.comments.push({
+    user: req.user._id,
+    text: comment,
+  });
+  await image.save(); // Save the updated document
+
+  res.status(200).json(new ApiResponse(200, image, "comment updated"));
+});
+
+const deleteImage = asyncHandler(async (req, res) => {
+  const { imageId } = req.params;
+
+  if (!imageId) {
+    throw new ApiError(400, "Image Id not provided");
+  }
+
+  const deletionResult = await Image.deleteOne({ imageId: imageId });
+
+  if (deletionResult.deletedCount === 0) {
+    throw new ApiError(404, "No image found with the given ID");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Image deleted successfully"));
+});
+
+// get all images for a particular albumId
+
+const getAllImagesByAlbumId = asyncHandler(async (req, res) => {
+  const { albumId } = req.params;
+  if (!albumId) {
+    throw new ApiError(400, "Album id is required");
+  }
+  const images = await Image.find({ albumId: albumId });
+  res
+    .status(200)
+    .json(new ApiResponse(200, images, "images fetched successfully"));
+});
+
+const getFavoriteImages = asyncHandler(async (req, res) => {
+  const { albumId } = req.params;
+  const favoriteImages = await Image.find({
+    albumId,
+    isFavorite: true,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, favoriteImages, "Fetched favorite Images"));
+});
+export {
+  uploadImage,
+  favoriteImage,
+  updateComment,
+  deleteImage,
+  getAllImagesByAlbumId,
+  getFavoriteImages,
+};
--- src/routes/images.routes.js
+++ src/routes/images.routes.js
@@ -1,11 +1,31 @@
 import { Router } from "express";
-
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.js";
-import { uploadImage } from "../controllers/image.controllers.js";
+import {
+  deleteImage,
+  favoriteImage,
+  getAllImagesByAlbumId,
+  getFavoriteImages,
+  updateComment,
+  uploadImage,
+} from "../controllers/image.controllers.js";
 
 const router = Router();
 
-router.route("/:albumId/images").post(verifyJWT,upload.single("image"), uploadImage)
+router
+  .route("/:albumId/images")
+  .post(verifyJWT, upload.single("image"), uploadImage);
+router
+  .route("/:albumId/images/:imageId/favorite")
+  .patch(verifyJWT, favoriteImage);
 
-export default router
+router
+  .route("/:albumId/images/:imageId/comments")
+  .patch(verifyJWT, updateComment);
+
+router.route("/:albumId/images/:imageId").delete(verifyJWT, deleteImage);
+router.route("/:albumId/images").get(verifyJWT, getAllImagesByAlbumId);
+
+router.route("/:albumId/images/favorites").get(verifyJWT, getFavoriteImages);
+
+export default router;
