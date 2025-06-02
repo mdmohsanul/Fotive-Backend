241dac3bc4115a7e2c66ae719ae8b8eda0825100
Mohsanul786 <mdmohsan2407@gmail.com>
2025-06-02 08:23:38 +0530

    fix: cookies options

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -167,7 +167,7 @@ const loginWithGoogle = asyncHandler(async (req, res) => {
     httpOnly: true,
     // secure: process.env.NODE_ENV === "production",
     secure: true,
-    sameSite: "strict",
+    sameSite: "None",
   };
 
   res
